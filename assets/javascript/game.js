06949c2dd718428a72a9cab73f91dc8558f564b5
Stevenson Nelson <sonson521@gmail.com>
2019-06-04 11:16:17 -0400

    added array to javascript.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,6 @@
-alert('hello')
+var gameArr = ['daria', 'gargoyles', 'doug', 'batman', 'xmen', 'arthur', 'spiderman', 'superman'];
+
+var firstKey = document.getElementById('startGame');
+document.onkeydown = function(event) {
+  firstKey.textContent = event.key;
+}
