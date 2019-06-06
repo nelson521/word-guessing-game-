c4dadfa7051a1900a30089e3bae8ee5892d072fd
Stevenson Nelson <sonson521@gmail.com>
2019-06-06 13:38:56 -0400

    just added guesses variable.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
-var gameArr = ['daria', 'gargoyles', 'doug', 'batman', 'xmen', 'arthur', 'spiderman', 'superman'];
+// The words 
+var words = ['daria', 'gargoyles', 'doug', 'batman', 'xmen', 'arthur', 'spiderman', 'superman', 'ironman', 'rugrats', 'beetlejuice', 'ducktales', 'talespin', 'catdog', 'pokemon', ''];
+// guesses that are okay
+var okGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var firstKey = document.getElementById('startGame');
-document.onkeydown = function(event) {
-  firstKey.textContent = event.key;
-}
+
+
