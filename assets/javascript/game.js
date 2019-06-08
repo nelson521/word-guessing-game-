9677a7514663d85a4021dfbad23c559cf4a74b0c
Stevenson Nelson <sonson521@gmail.com>
2019-06-07 22:25:30 -0400

    added current number and guesses to function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,30 @@
 // The words 
-var words = ['daria', 'gargoyles', 'doug', 'batman', 'xmen', 'arthur', 'spiderman', 'superman', 'ironman', 'rugrats', 'beetlejuice', 'ducktales', 'talespin', 'catdog', 'pokemon', ''];
+var words = ['daria', 'gargoyles', 'doug', 'batman', 'xmen', 'arthur', 'spiderman', 'superman', 'ironman', 'rugrats', 'beetlejuice', 'ducktales', 'talespin', 'catdog', 'pokemon'];
 // guesses that are okay
 var okGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
+document.onkeyup = function (event) {
+  var pressed = event.key;
+  document.getElementById("wins");
+  var winSpan = document.getElementById("wins");
+  winSpan.textContent = "0";
+  var cWord = document.getElementById('currentWord');
+  // current word loop 
+for (var i =0; i < words[0].length;i++) {
+  // document.getElementById("currentWord");
+  // make the word random
+  var word = Math.random()
+  
+  
+  var node = document.createElement('span');
+  var textNode = document.createTextNode('_');
+  node.appendChild(textNode);
+  cWord.appendChild(node);
+}
+// number of guesses 
+var numGuess = document.getElementById('numOfGuess');
+numGuess.textContent = '14';
 
+}
+  
